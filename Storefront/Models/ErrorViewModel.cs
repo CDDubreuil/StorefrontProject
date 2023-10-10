1a962ca2bf61045675aa24f82dfe0f2f792fb2c2
Chanel Dubreuil <cddubreuil@outlook.com>
2023-10-09 21:07:43 -0500

    Add project files.

---
+++ Storefront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Storefront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
